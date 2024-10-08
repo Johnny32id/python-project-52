f9ec76787f18acb98acea6c1a71ee54932f6f849
Johnny32id <johnnyhandsome3232@gmail.com>
2024-10-08 23:18:06 +0300

    added main page

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.utils.translation import gettext_lazy as _
 
 
 def index(request):
-    return render(request, 'index.html', context={
-        'who': 'World',
-    })
+    return HttpResponse("Hello, world. You're at the pollapp index.")
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+    extra_context = {'header': _('Task manager')}
