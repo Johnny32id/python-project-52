8510cbda8b112b2fe7bc379438f095151c50d56b
Johnny32id <johnnyhandsome3232@gmail.com>
2024-10-08 21:03:53 +0300

    try

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['https://python-project-52-u1nd.onrender.com',
-                 'webserver', '127.0.0.1', 'localhost', '0.0.0.0']
+                 'webserver', '127.0.0.1', 'localhost']
 
 
 # Application definition
