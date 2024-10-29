33d01a11caaaceeb078d6f8493e7f9965aefba8e
Johnny32id <johnnyhandsome3232@gmail.com>
2024-10-29 13:19:27 +0300

    changed path

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'task_manager/static')
 
 # This production code might break development mode, so we check whether we're
 # in DEBUG mode
