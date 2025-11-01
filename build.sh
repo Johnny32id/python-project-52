6882964d8a9a5ff6f037372229987778a4aa9ce3
johnny32id <johnnyhandsome3232@gmail.com>
2025-11-01 15:14:21 +0300

    fix: добавлена настройка Poetry для использования Python из PYTHON_VERSION

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 # Exit on error
 set -o errexit
 
+# Ensure Poetry uses the correct Python version
+if [ -n "$PYTHON_VERSION" ]; then
+    python_path=$(which python3 || which python)
+    poetry env use "$python_path" || poetry env use "$PYTHON_VERSION"
+fi
+
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 make install
 
